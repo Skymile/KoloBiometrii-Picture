311a9da560a69831e1962caa337fc6878ab986b8
Patryk Milewski <mpatryk97@gmail.com>
2018-12-10 19:06:19 +0100

    Functional KMM

--- Models/Picture.KMM.cs
+++ Models/Picture.KMM.cs
@@ -39,9 +39,6 @@ namespace Models
 
             public static void Apply(byte* ptr, int stride, int height)
             {
-                int[] neighbours = Neighbours(stride);
-                List<int> ones = GetBlackPixels(ptr, stride * height, stride + 3);
-
                 bool AreSidesZeroes(int i) =>
                     ptr[i + 3] == Zero || ptr[i + stride] == Zero ||
                     ptr[i - 3] == Zero || ptr[i - stride] == Zero;
@@ -50,12 +47,6 @@ namespace Models
                     ptr[i + stride + 3] == Zero || ptr[i + stride - 3] == Zero ||
                     ptr[i - stride + 3] == Zero || ptr[i - stride - 3] == Zero);
 
-                Func<int, bool> Contains(HashSet<int> set) => 
-                    i => set.Contains(ComputeSum(i));
-
-                Func<int, bool> IsValueToDelete(byte value) =>
-                    i => ptr[i] == value && Deletion.Contains(ComputeSum(i));
-
                 Action<int> Stage(Func<int, bool> condition, byte setTo) =>
                     i =>
                     {
@@ -63,14 +54,24 @@ namespace Models
                             ptr[i] = ptr[i + 1] = ptr[i + 2] = setTo;
                     };
 
+                int[] neighbours = Neighbours(stride);
+
+                Func<int, bool> Contains(HashSet<int> set) => 
+                    i => set.Contains(ComputeSum(i));
+
+                Func<int, bool> IsValueToDelete(byte value) =>
+                    i => ptr[i] == value && Deletion.Contains(ComputeSum(i));
+
+                List<int> ones = GetBlackPixels(ptr, stride * height, stride + 3);
+
                 var sequence = new List<Action<int>>
                 {
-                    Stage(AreSidesZeroes, Two),
-                    Stage(AreCornersZeroes, Three),
-                    Stage(IsValueToDelete(Four), Four),
-                    Stage(IsValueToDelete(Four), Zero),
-                    Stage(IsValueToDelete(Two), Zero),
-                    Stage(IsValueToDelete(Three), Zero)
+                    Stage(condition: AreSidesZeroes        , setTo: Two),
+                    Stage(condition: AreCornersZeroes      , setTo: Three),
+                    Stage(condition: Contains(Fours)       , setTo: Four),
+                    Stage(condition: IsValueToDelete(Four) , setTo: Zero),
+                    Stage(condition: IsValueToDelete(Two)  , setTo: Zero),
+                    Stage(condition: IsValueToDelete(Three), setTo: Zero)
                 };
 
                 int count = 0;
@@ -108,11 +109,11 @@ namespace Models
                  32, 16, 8
             };
 
-            private const byte Zero = 255;
-            private const byte One = 0;
-            private const byte Two = 2;
-            private const byte Three = 3;
-            private const byte Four = 4;
+            private const byte Zero  = 255;
+            private const byte One   =   0;
+            private const byte Two   =   2;
+            private const byte Three =   3;
+            private const byte Four  =   4;
 
             private static readonly HashSet<int> Deletion = new HashSet<int>(new int[] { 3, 5, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30, 31, 48, 52, 53, 54, 55, 56, 60, 61, 62, 63, 65, 67, 69, 71, 77, 79, 80, 81, 83, 84, 85, 86, 87, 88, 89, 91, 92, 93, 94, 95, 97, 99, 101, 103, 109, 111, 112, 113, 115, 116, 117, 118, 119, 120, 121, 123, 124, 125, 126, 127, 131, 133, 135, 141, 143, 149, 151, 157, 159, 181, 183, 189, 191, 192, 193, 195, 197, 199, 205, 207, 208, 209, 211, 212, 213, 214, 215, 216, 217, 219, 220, 221, 222, 223, 224, 225, 227, 229, 231, 237, 239, 240, 241, 243, 244, 245, 246, 247, 248, 249, 251, 252, 253, 254, 255 });
             private static readonly HashSet<int> Fours = new HashSet<int>(new int[] { 3, 6, 12, 24, 48, 96, 192, 129, 7, 14, 28, 56, 112, 224, 193, 131, 15, 30, 60, 120, 240, 225, 195, 135 });
