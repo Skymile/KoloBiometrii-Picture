bb503fc7d09d4cb35d2761e2a3b9624545cf4bd6
Patryk Milewski <mpatryk97@gmail.com>
2019-04-02 16:21:16 +0200

    Prepared

---
+++ Paint/NativeMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Models
+{
+    internal static class NativeMethods
+    {
+        internal static BitmapSource GetBitmapSource(this Bitmap bitmap)
+        {
+            IntPtr ptr = bitmap.GetHbitmap();
+            try
+            {
+                return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    ptr, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions()
+                );
+            }
+            finally
+            {
+                DeleteObject(ptr);
+            }
+        }
+
+        [DllImport("gdi32.dll")]
+        private static extern bool DeleteObject(IntPtr ptr);
+    }
+}
