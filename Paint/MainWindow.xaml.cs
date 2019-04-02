ea9e0eab30ca4235cf50cf03329a80a2a2807d67
Patryk Milewski <mpatryk97@gmail.com>
2019-04-02 18:29:54 +0200

    Set WPF Binding

--- Paint/MainWindow.xaml.cs
+++ Paint/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +20,27 @@ namespace Paint
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow : Window, INotifyPropertyChanged
 	{
 		public MainWindow()
 		{
 			InitializeComponent();
 		}
+
+		public byte R { get => r; set => Set(value, ref r); }
+		public byte G { get => g; set => Set(value, ref g); }
+		public byte B { get => b; set => Set(value, ref b); }
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void Set<T>(T value, ref T field, [CallerMemberName] string name = "")
+		{
+			field = value;
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+
+		private byte r;
+		private byte g;
+		private byte b;
 	}
 }
