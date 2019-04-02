143184a61074e5c84ffb81d6fcb4884332a8909c
Patryk Milewski <mpatryk97@gmail.com>
2019-04-02 19:12:09 +0200

    Local Fill

--- Paint/MainWindow.xaml.cs
+++ Paint/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -33,6 +34,8 @@ namespace Paint
 		public byte R { get => r; set => SetChannel(value, ref r); }
 		public byte G { get => g; set => SetChannel(value, ref g); }
 		public byte B { get => b; set => SetChannel(value, ref b); }
+
+		public byte Threshold { get => threshold; set => Set(value, ref threshold); }
 
 		public System.Windows.Media.Brush Fill => new SolidColorBrush(System.Windows.Media.Color.FromRgb(R, G, B));
 
@@ -67,6 +70,7 @@ namespace Paint
 		private byte r;
 		private byte g;
 		private byte b;
+		private byte threshold;
 
 		private void Image_MouseMove(object sender, MouseEventArgs e)
 		{
@@ -78,7 +82,6 @@ namespace Paint
 					X, Y, 
 					System.Drawing.Color.FromArgb(R, G, B)
 				);
-				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(picture)));
 				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MainSource)));
 			}
 		}
@@ -91,6 +94,80 @@ namespace Paint
 			int y = (int)(p.Y / img.ActualHeight * picture.Height);
 
 			return (x, y);
+		}
+
+		private unsafe void Image_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (this.CurrentTool == ToolType.LocalFill)
+			{
+				BitmapData data = picture.LockBits(
+					new System.Drawing.Rectangle(System.Drawing.Point.Empty, picture.Size), 
+					ImageLockMode.ReadWrite,
+					picture.PixelFormat
+				);
+
+				byte* p = (byte*)data.Scan0.ToPointer();
+
+				(int X, int Y) = GetPosition((System.Windows.Controls.Image)sender, e);
+
+				int bpp = System.Drawing.Image.GetPixelFormatSize(picture.PixelFormat) / 8;
+				int stride = picture.Width * bpp;
+				int length = stride * picture.Height;
+
+				int current = X * bpp + Y * stride;
+
+				(byte R, byte G, byte B) value = (p[current], p[current + 1], p[current + 2]);
+
+				var visited = new HashSet<int>();
+				visited.Add(current);
+
+				int[] offsets =
+				{
+					stride, -stride, bpp, -bpp, 
+					stride - bpp, stride + bpp, -stride - bpp, -stride + bpp
+				};
+
+				bool anyChange = true;
+				while (anyChange)
+				{
+					anyChange = false;
+
+					var newValues = new List<int>();
+					foreach (int i in visited)
+					{
+						foreach (int o in offsets)
+						{
+							if (!visited.Contains(i + o) &&
+								i + o > 0 && i + o + 2 < length &&
+
+								p[i + o + 0] > value.R - threshold &&
+								p[i + o + 0] < value.R + threshold &&
+								p[i + o + 1] > value.G - threshold &&
+								p[i + o + 1] < value.G + threshold &&
+								p[i + o + 2] > value.B - threshold &&
+								p[i + o + 2] < value.B + threshold
+							)
+							{
+								newValues.Add(i);
+								anyChange = true;
+							}
+						}
+					}
+
+					foreach (int i in newValues)
+						visited.Add(i);
+				}
+
+				foreach (int i in visited)
+				{
+					p[i + 0] = R;
+					p[i + 1] = G;
+					p[i + 2] = B;
+				}
+
+				picture.UnlockBits(data);
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MainSource)));
+			}
 		}
 	}
 
